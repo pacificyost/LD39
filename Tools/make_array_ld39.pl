68795da2310b4c7bcc610188523c2cf815d9bbcb
pacificyost <pacificyost@gmail.com>
2017-07-30 17:46:36 -0700

    Updated room generation perl
    Updated room generation perl script to include mines also and removed
    the extra column and row that was not being displayed.

--- Tools/make_array_ld39.pl
+++ Tools/make_array_ld39.pl
@@ -1,19 +1,21 @@
-#Use this perl script to generate random arrays for static levels
-
 open ARRAY,">", "array_txt.txt";
 
-for ($x = 2; $x <= 30; $x++)
+for ($x = 2; $x <= 29; $x++)
 {
-	for ($y = 2; $y <= 22; $y++)
+	for ($y = 2; $y <= 21; $y++)
 	{
 		$type = rand(100);
 		if ($type <= 1)
 		{
 			print ARRAY "object[$x, $y] = obj_energy;\n";
 		}
-		elsif ($type >= 60)
+		elsif ($type >= 70)
 		{
 			print ARRAY "object[$x, $y] = obj_wall;\n";
+		}
+		elsif ($type >= 65 and $type < 70)
+		{
+			print ARRAY "object[$x, $y] = obj_mine;\n";
 		}
 		else
 		{
